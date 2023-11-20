1f96aecf981ac89e64ef49e535d8d576c21cc671
ALEXANDRA ARDELEAN <alexandra.ardelean1@stud.ubbcluj.ro>
2023-11-20 20:13:50 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Ardelean_Alexandra_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ardelean_Alexandra_Lab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
